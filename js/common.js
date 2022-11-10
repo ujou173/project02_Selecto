398a2af582c110cf611f44d166f31e4402650e48
ujou173 <ujou173@gmail.com>
2022-11-10 15:24:35 +0900

    2 SELECTO

---
+++ js/common.js
@@ -0,0 +1,29 @@
+$(function () {
+  var sec1_swiper = new Swiper(".main_banner_slide", {
+    loop: true,
+    pagination: {
+      el: ".sec1_pg",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  });
+
+  var menu_swiper = new Swiper(".sec3_slide", {
+    slidesPerView: 4,
+    spaceBetween: 16,
+    loop: true,
+    navigation: {
+      nextEl: ".sec3_next",
+      prevEl: ".sec3_prev",
+    },
+  });
+
+  var sec4_swiper = new Swiper(".real_interview", {
+    loop: true,
+    pagination: {
+      el: ".sec4_pg",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  });
+});
