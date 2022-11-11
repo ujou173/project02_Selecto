5cfad492f3158b3cafd672528309dc3606daafc8
ujou173 <ujou173@gmail.com>
2022-11-11 17:57:12 +0900

    pc버전 완성 / 반응형 적용 시작

--- js/common.js
+++ js/common.js
@@ -1,6 +1,10 @@
 $(function () {
   var sec1_swiper = new Swiper(".main_banner_slide", {
     loop: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".sec1_pg",
       clickable: true,
@@ -9,9 +13,19 @@ $(function () {
   });
 
   var menu_swiper = new Swiper(".sec3_slide", {
-    slidesPerView: 4,
+    slidesPerView: 2,
     spaceBetween: 16,
     loop: true,
+    breakpoints: {
+      670: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      850: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
     navigation: {
       nextEl: ".sec3_next",
       prevEl: ".sec3_prev",
@@ -20,10 +34,21 @@ $(function () {
 
   var interview_swiper = new Swiper(".real_interview", {
     loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".sec4_pg",
       clickable: true,
       dynamicBullets: true,
     },
+  });
+
+  $(".sec3_btns > button").click(function () {
+    let btnIndex = $(this).index();
+    $(".sec3_btns > button, .menu_con").removeClass("on");
+    $(this).addClass("on");
+    $(".menu_con").eq(btnIndex).addClass("on");
   });
 });
